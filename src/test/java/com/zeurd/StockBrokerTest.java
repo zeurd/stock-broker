6f07da8d5768da9c94ea01b22420dfec99848fa1
zeurd <zeurd>
2020-11-20 17:35:31 +0000

    Initial commit

---
+++ src/test/java/com/zeurd/StockBrokerTest.java
@@ -0,0 +1,21 @@
+package com.zeurd;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class StockBrokerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
